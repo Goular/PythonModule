3e6342b179b27f363ccd5205d1dd30ca06c4b3ec
Goular <zhaojt_cs@126.com>
2017-12-31 00:31:48 +0800

    学习内容提交

--- 02.天善-Python数据分析与挖掘实战/02.网络爬虫基础/04.匹配.com或.cn网址.py
+++ 02.天善-Python数据分析与挖掘实战/02.网络爬虫基础/04.匹配.com或.cn网址.py
@@ -1,6 +1,6 @@
 import re
 
 str = "<a href='http://www.baidu.com'>百度一下，我就知道</a>"
-pat = '[a-zA-Z]+://[^\s]+[.com|.cn]'
+pat = '[a-zA-Z]+://[^\s]+[.com|.cn]' # 其实这个是有错误的写法，但是在这里列子中可以实现鉴别那就算了，因为中括号的内容是平等的
 rst = re.compile(pat).findall(str)
 print(rst)
